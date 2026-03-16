a22b51d911c08fbb9a88c9126b3938929ef65619
Larissa <larissacamyle4@gmail.com>
2026-03-16 20:55:30 -0300

    Finalizando o projeto

--- javascript.js
+++ javascript.js
@@ -1,10 +1,61 @@
+//====================================================================
+//VERIFICAÇÃO DO FORMULÁRIO PARA SIMULAR UM ENVIO DE E-MAIL
+//====================================================================
+
+//button de enviar e-mail localizado no formulário na aba de contato
+const botao = document.getElementById("btn_enviar_formulario");
+
+//quando o button for clicado a função abaixo vai executar
+botao.addEventListener("click", function () {
+    //=========================================================
+    //SELECIONA OS ELEMENTOS DO FORMULÁRIO 
+    //=========================================================
+    //campo do input referente ao nome
+    const nome = document.querySelector("#formulario_nome");
+    //campo do input referente ao e-mail
+    const email = document.querySelector("#formulario_email");
+    //campo do input referente a mensagem
+    const mensagem = document.querySelector("#formulario_mensagem");
+
+    //=========================================================
+    //VALIDA OS DADOS INSERIDOS NO FORMULÁRIO 
+    //=========================================================    
+    //verifica se algum campo está vazio, se estiver finaliza a função com o return
+    if (nome.value === "" || email.value === "" || mensagem.value === "") {
+        alert("Preencha todos os campos corretamente!");
+        return;
+    }
+
+    //verifica se o e-mail digitado possui o @, se não possuir @ finaliza a função com o return
+    if (!email.value.includes("@")) {
+        alert("Digite um e-mail válido!");
+        return;
+    }
+
+    //=========================================================
+    //SELECIONA OS ELEMENTOS DO FORMULÁRIO 
+    //=========================================================
+    //mensagem de sucesso
+    alert("Mensagem enviada com sucesso!");
+
+    //limpa os campos após simular o envio de e-mail
+    nome.value = "";
+    email.value = "";
+    mensagem.value = "";
+});
+
+
+//====================================================================
+//FUNÇÃO QUE REALIZA A TROCA DAS IMAGENS DOS PROJETOS
+//====================================================================
+
 //lista que armazena o número do projeto, o nome do projeto
 //e a quantidade de imagens que cada projeto tem, para poder realizar
 //a troca de imagens da maneira correta
 const listaElementosProjeto = [
     {
         numeroDoProjeto: 1,
-        nome: "NumeroDoProjeto Estoque",
+        nome: "Estoque",
         quantidadeImagens: 2
     },
     {
@@ -76,7 +127,7 @@ function TrocaImagens(){
         //variavel de contagem para realizar a troca das imagens
         let contagem = 1;
 
-        //funcao responsavel por repetir a cada 4 segundos
+        //funcao responsavel por repetir a cada 3 segundos
         //funcao troca a imagem no html
         setInterval(() => {
             //nome do caminho da imagem de acordo com o projeto
@@ -92,26 +143,10 @@ function TrocaImagens(){
             if(contagem > quantidadeImagens){
                 contagem = 1;
             }
-        }, 3000);// troca as imagens a cada 4 segundos
+        }, 3000);// troca as imagens a cada 3 segundos
 
     })
 }
 
+//Inicializa a função Troca de Imagens assim que a página HTML iniciar
 TrocaImagens();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
